c9dd8d7937e8879aaefe648db039c444beb026c9
Ewerton <3w3rt0n@gmail.com>
2017-01-03 16:33:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(request, response){
+	
 	response.writeHead(200, {"Content-Type": "text/html"});
+	
 	if(request.url == "/"){
 		response.write("<h1>Página principal</h1>");
 		console.log('Pagina inicial');
 	}else if(request.url == "/bemvindo"){
 		response.write("<h1>Bem-vindo :)</h1>");
 		console.log('Pagina bem vindo');
-	}else{
+	}else if(request.url == "/dispositivos"){
+		fs.readFile(__dirname + '/HTML/dispositivos.html', function(err, html){
+			response.write(html);		
+		});
+		console.log('Arquivo dispositivos.');
+	}else {
 		response.write("<h1>Página não encontrada :(</h1>");
 		console.log('Pagina nao encontrada');
 	}
 	response.end();
 });
 
-server.listen(process.env.PORT || 5000, function(){
+server.listen(process.env.PORT, function(){
 	console.log('Servidor rodando!');
 });
