cbe28de94745bc297fcf356ade5d80700f21b882
Ewerton <3w3rt0n@gmail.com>
2017-01-03 16:45:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,21 +8,25 @@ var server = http.createServer(function(request, response){
 	if(request.url == "/"){
 		response.write("<h1>Página principal</h1>");
 		console.log('Pagina inicial');
+		response.end();
 	}else if(request.url == "/bemvindo"){
 		response.write("<h1>Bem-vindo :)</h1>");
 		console.log('Pagina bem vindo');
+		response.end();
 	}else if(request.url == "/dispositivos"){
 		console.log('Arquivo dispositivos - inicio.');
 		fs.readFile(__dirname + '/HTML/dispositivos.html', function(err, html){
 			response.write(html);
 			console.log(html);
+			response.end();
 		});
 		console.log('Arquivo dispositivos - fim.');
 	}else {
 		response.write("<h1>Página não encontrada :(</h1>");
 		console.log('Pagina nao encontrada');
+		response.end();
 	}
-	response.end();
+	
 });
 
 server.listen(process.env.PORT, function(){
