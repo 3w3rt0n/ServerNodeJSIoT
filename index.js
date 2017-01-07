de6faae6c471e1139a044539ac059bfbd8b2a08f
Ewerton <3w3rt0n@gmail.com>
2017-01-07 19:04:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,21 @@ var server = http.createServer(function(request, response){
 	
 	response.writeHead(200, {"Content-Type": "text/html"});
 	
-	if(request.url == "/"){
+	if (request.method == 'POST') {
+        	var body = '';
+        	request.on('data', function (data) {
+	            	body += data;
+        	    	// Too much POST data, kill the connection!
+            		// 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            		if (body.length > 1e6)
+	                	request.connection.destroy();
+        	});
+
+        	request.on('end', function () {
+            		console.log(body); // should work
+            		// use post['blah'], etc.
+        	});
+    	}else if(request.url == "/"){
 		response.write("<h1>Página principal</h1>");
 		console.log('Pagina inicial');
 		response.end();
