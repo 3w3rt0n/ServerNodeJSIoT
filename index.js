30457043ef3759a2d152a1991eddba2aa8d67cb1
Ewerton <3w3rt0n@gmail.com>
2017-01-07 20:04:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -40,11 +40,11 @@ var server = http.createServer(function(request, response){
         	var body = '';
     		request.on('data', function(chunk) {
       			body += chunk;
-			console.log(body);
     		});
     		request.on('end', function() {
       			var data = qs.parse(body);
 			console.log(data.email);
+			console.log(data.pwd);
       			// now you can access `data.email` and `data.password`
    		});
     	}else if(request.url == "/"){
